efeac8c6572d8835f96f6b0a4139b586ce2c5e2f
Anand-Mobile-And-Electrics <aks0317electronics@gmail.com>
2026-07-05 15:25:06 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
 import "./globals.css";
@@ -48,6 +49,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             </div>
           </nav>
         </header>
+       <Script
+  async
+  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6997490039969865"
+  crossOrigin="anonymous"
+  strategy="afterInteractive"
+/>
         {children}
       </body>
     </html>
